c0043def6eeef52073e6918adfcb626e09b4201a
shshaheen <kolimishaheen9885@gmail.com>
2025-01-16 10:09:34 +0530

    Taking input,triggering through controller and saving it to display on the debug console

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -7,20 +7,57 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+    final _amountController = TextEditingController();
+    @override
+    void dispose() {
+      _titleController.dispose();
+      _amountController.dispose();
+      super.dispose();
+    }
   @override
   Widget build(BuildContext context) {
+    
+
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             // keyboardType: TextInputType.numberWithOptions(),
             decoration: InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            
+            decoration: InputDecoration(
+              prefixText: '\$',
+              label: Text('Amount'),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: (){},
+               child: Text("Cancel"),
+               ),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text("Save Expense")),
+              SizedBox(width: 5.0),
+              
+            ],
+          )
         ],
-      ),);
+      ),
+    );
   }
 }
